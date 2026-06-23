20e41ea2e9329572de33c9409f3e2da2e85056c1
Lars Ultsch <lcu4android@gmail.com>
2026-06-23 11:58:53 +0200

    feat: Add Cli prompting

--- src/agent/connector.py
+++ src/agent/connector.py
@@ -4,14 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
+# Prepare LLM chain
 llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash")
+chain = llm | StrOutputParser()
 
+# Read system prompt
 with open("src/prompts/SYSTEM_PROMPT.md", "r", encoding="UTF-8") as file:
     system_prompt = file.read()
 
-prompts = [("system", system_prompt), ("human", "")]
+def send_request(user_prompt: str):
+    """Sends the user prompt and the defined system prompt to the LLM. Streams the output to the console."""
+    prompts = [("system", system_prompt), ("human", user_prompt)]
 
-chain = llm | StrOutputParser()
-
-for chunk in chain.stream(prompts):
-    print(chunk, end="", flush=True)
+    for chunk in chain.stream(prompts):
+        print(chunk, end="", flush=True)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from kids-demo!")
+import typer
+from src.agent.connector import send_request
 
+app = typer.Typer()
+
+@app.command()
+def invoke_llm(
+    prompt: str = typer.Argument(help="The User prompt")
+):
+    """
+    Prompt the kids demo LLM
+    """
+    send_request(prompt)
 
 if __name__ == "__main__":
-    main()
+    app()
